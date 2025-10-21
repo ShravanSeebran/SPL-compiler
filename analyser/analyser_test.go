a71614e64f620125667a307414d7ebdf8cfc82d3
Chris <christopherxides@gmail.com>
2025-10-21 18:52:45 +0200

    Add simple analyser test cases

---
+++ analyser/analyser_test.go
@@ -0,0 +1,67 @@
+package analyser
+
+import (
+	"SPL-compiler/lexer"
+	"SPL-compiler/parser"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestSymbolTable(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+	}{
+		{"Redeclaration of global variables in main scope", `
+		  glob { x y }
+		  proc {}
+		  func {}
+		  main { 
+			var { x z } 
+			halt 
+		  }
+		`},
+		{"Using global variable in local function scope", `
+		  glob { x y }
+		  proc { f(a b) { local { c } x = a } g(a b) { local { c } x = a } }
+		  func {}
+		  main { 
+			var { x z } 
+			halt 
+		  }
+		`},
+	}
+	for _, tt := range tests {
+		fmt.Println("\n-------------- ", tt.name, " --------------")
+		fmt.Println(tt.input)
+		ast := testParse(tt.input)
+		testProgram(ast)
+
+	}
+}
+
+func testParse(input string) *parser.ASTNode {
+	l := lexer.New(input)
+	lexerAdapter := &parser.LexerAdapter{L: l}
+	result, err := parser.Parse(lexerAdapter)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Parsing error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if result != nil {
+		fmt.Println("\n--- Abstract Syntax Tree ---")
+		parser.PrintAST(result, 0)
+	} else {
+		fmt.Fprintf(os.Stderr, "Parsing finished, but no AST was generated.\n")
+	}
+
+	return parser.ResultAST
+}
+
+func testProgram(ast *parser.ASTNode) {
+	analyseProgram(ast)
+	fmt.Println("\n---Symbol Table ---")
+	PrettyPrintSymbolTable(symbolTable)
+}
--- analyser/symbolTable.go
+++ analyser/symbolTable.go
@@ -1,5 +1,11 @@
 package analyser
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 type SemanticInfo struct {
 	nodeID          int
 	symbolName      string
@@ -9,3 +15,71 @@ type SemanticInfo struct {
 }
 
 type SymbolTable map[int]SemanticInfo
+
+func PrettyPrintSymbolTable(st SymbolTable) {
+	if len(st) == 0 {
+		fmt.Println("Symbol table is empty")
+		return
+	}
+
+	keys := make([]int, 0, len(st))
+	for k := range st {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	widths := struct {
+		nodeID, symbolName, dataType, scopeLevel, declarationNode int
+	}{
+		nodeID:          6,
+		symbolName:      11,
+		dataType:        9,
+		scopeLevel:      11,
+		declarationNode: 16,
+	}
+
+	for _, info := range st {
+		if len(info.symbolName) > widths.symbolName {
+			widths.symbolName = len(info.symbolName)
+		}
+		if len(info.dataType) > widths.dataType {
+			widths.dataType = len(info.dataType)
+		}
+		if len(fmt.Sprint(info.nodeID)) > widths.nodeID {
+			widths.nodeID = len(fmt.Sprint(info.nodeID))
+		}
+		if len(fmt.Sprint(info.scopeLevel)) > widths.scopeLevel {
+			widths.scopeLevel = len(fmt.Sprint(info.scopeLevel))
+		}
+		if len(fmt.Sprint(info.declarationNode)) > widths.declarationNode {
+			widths.declarationNode = len(fmt.Sprint(info.declarationNode))
+		}
+	}
+
+	separator := fmt.Sprintf("+-%s-+-%s-+-%s-+-%s-+-%s-+",
+		strings.Repeat("-", widths.nodeID),
+		strings.Repeat("-", widths.symbolName),
+		strings.Repeat("-", widths.dataType),
+		strings.Repeat("-", widths.scopeLevel),
+		strings.Repeat("-", widths.declarationNode))
+
+	fmt.Println(separator)
+	fmt.Printf("| %-*s | %-*s | %-*s | %-*s | %-*s |\n",
+		widths.nodeID, "NodeID",
+		widths.symbolName, "Symbol Name",
+		widths.dataType, "Data Type",
+		widths.scopeLevel, "Scope Level",
+		widths.declarationNode, "Declaration Node")
+	fmt.Println(separator)
+
+	for _, key := range keys {
+		info := st[key]
+		fmt.Printf("| %-*d | %-*s | %-*s | %-*d | %-*d |\n",
+			widths.nodeID, info.nodeID,
+			widths.symbolName, info.symbolName,
+			widths.dataType, info.dataType,
+			widths.scopeLevel, info.scopeLevel,
+			widths.declarationNode, info.declarationNode)
+	}
+	fmt.Println(separator)
+}
